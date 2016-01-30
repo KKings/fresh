1dd08776d6d29830ede90f05ba05fb1a7f8b8431
Kyle Kingsbury <kyle.l.kingsbury@gmail.com>
2016-01-30 12:55:21 -0500

    Adds factor contracts

---
+++ src/Feature/fresh/code/Services/FactorArgs.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Sitecore.Feature.Indicator.Services
+{
+    using Data.Items;
+
+    /// <summary>
+    /// Arguments for resolving a factor
+    /// </summary>
+    public class FactorArgs
+    {
+        /// <summary>
+        /// Gets the Item
+        /// </summary>
+        public Item Item { get; private set; }
+
+        public FactorArgs(Item item)
+        {
+            this.Item = item;
+        }
+    }
+}
---
+++ src/Feature/fresh/code/Services/IFactor.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sitecore.Feature.Indicator.Services
+{
+    /// <summary>
+    /// Factor implementation for resolving a factor
+    /// </summary>
+    public interface IFactor
+    {
+        /// <summary>
+        /// Resolves the boost for the factor
+        /// </summary>
+        /// <param name="args">The args</param>
+        /// <returns><c>Float</c> indicating the boost fro the factor</returns>
+        float Boost(FactorArgs args);
+    }
+}
