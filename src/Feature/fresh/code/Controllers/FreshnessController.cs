dd242d221f3a9b4473ee6e2065324cc30a1acc5c
Lemaire <lstewartvt@gmail.com>
2016-01-30 18:09:40 -0500

    Add freshometer rendering

---
+++ src/Feature/fresh/code/Controllers/FreshnessController.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Feature.Indicator.Controllers
+{
+    using System.Web.Mvc;
+    using Mvc.Controllers;
+    using Pipelines.Freshness;
+    using Sitecore.Pipelines;
+
+    public class FreshnessController : SitecoreController
+    {
+        public ActionResult Freshometer()
+        {
+            var args = new FreshnessArgs(Sitecore.Context.Item);
+            CorePipeline.Run("freshness", args, false);
+
+            return View(args.FreshnessRating);
+        }
+    }
+}
---
+++ src/Feature/fresh/code/Factors/PageTrafficFactor.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Data;
+using Sitecore.Analytics.Data.DataAccess;
+
+namespace Sitecore.Feature.Indicator.Factors
+{
+    using System;
+    using Data.Fields;
+    using Diagnostics;
+
+    using Sitecore.Analytics;
+    using Sitecore.Analytics.Data.DataAccess;
+
+    /// <summary>
+    /// Boost Factor Implementation for Freshness by Updated Date
+    /// </summary>
+    public class PageTrafficFactor : IFactor
+    {
+        /// <summary>
+        /// Resolves the boost by calculating how fresh the content is 
+        /// by the Current Date minus the last updated date
+        /// </summary>
+        /// <param name="args">The args</param>
+        /// <returns><c>Boost</c> value</returns>
+        public float Boost(FactorArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+        }
+    }
+}
